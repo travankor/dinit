92f4f71ae4b09668e30d94c2b8dd2ff362220318
Davin McCall <davmac@davmac.org>
2019-08-10 14:20:40 +1000

    WIP

---
+++ src/dinitcheck.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// dinitcheck:  utility to check Dinit configuration for correctness/lint
+
+int main(int argc, char **argv)
+{
+    using namespace std;
+
+    // Figure out service dirs
+    // Load named service(s)
+
+    return 0;
+}
